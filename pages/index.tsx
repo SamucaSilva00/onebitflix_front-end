181f9185779aab54eb1289ef65e9588761ec0c0b
SamucaSilva00 <samuel.rodrigueslsilva@gmail.com>
2024-07-04 10:31:52 -0300

    finalização do Header e do componente de Apresentação, ambos da tela inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import Head from "next/head";
-import styles from "../styles/homeNoAuth.module.scss" 
+import styles from "../styles/homeNoAuth.module.scss"; 
+import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
+import PresentationSection from "@/components/homeNoAuth/presentationSection";
 
 const HomeNotAuth = function () {
   return (
-		<>
-			<Head>Home</Head>
-			<main></main>
-		</>
-
-  )
+    <>
+      <Head>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
+      </Head>
+      <main>
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default HomeNotAuth;
---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import React from "react";
+import { Button, Container } from "reactstrap";
+
+const HeaderNoAuth = function () {
+  return (
+    <>
+        <div className={styles.ctaSection}>
+            <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={styles.imgCta}/>
+            <p>Se cadastre para ter acesso aos cursos</p>
+            <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={styles.imgCta}/>
+        </div>
+        <Container className={styles.nav}>
+          <img src="/logoOnebitflix.svg" alt="logoOnebitflix" className={styles.imgLogoNav}/>
+          <div>
+            <Link href="/login">
+              <Button className={styles.navBtn} outline>Entrar</Button>
+            </Link>
+            <Link href="/register">
+              <Button className={styles.navBtn} outline>Quero fazer parte</Button>
+            </Link>
+          </div>
+        </Container>
+    </>
+  );
+};
+
+export default HeaderNoAuth;
