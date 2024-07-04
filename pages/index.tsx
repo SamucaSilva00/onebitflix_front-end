af032d09975276681a8ffb10c01064259a2a3936
SamucaSilva00 <samuel.rodrigueslsilva@gmail.com>
2024-07-04 07:36:17 -0300

    Iniciando o FrontEnd - Configurando o projeto, as pastas e instalando as libs necessárias

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss" 
+
+const HomeNotAuth = function () {
+  return (
+		<>
+			<Head>Home</Head>
+			<main></main>
+		</>
+
+  )
+};
+
+export default HomeNotAuth;
